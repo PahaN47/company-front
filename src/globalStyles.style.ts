e0f017405b6009ba7225bd0a72f8000dd35e3eb4
PahaN47 <pahan4781@gmail.com>
2023-03-01 01:41:20 +0300

    test commit

---
+++ src/globalStyles.style.ts
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    html {
+        margin: 0;
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+        min-height: 100%;
+        width: 100%;
+        overflow-x: hidden;
+    }
+
+    div, 
+    form,
+    input,
+    th,
+    td,
+    textarea,
+    ul {
+        margin: 0;
+    }
+
+    ul {
+       padding-left: 0; 
+    }
+
+    li {
+        list-style-type: none;
+    }
+
+    a, 
+    button {
+        outline: none;
+        cursor: pointer;
+    }
+
+    input,
+    textarea {
+        outline: none;
+    }
+`;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from "react";
+import { AppProps } from "next/app";
+import { GlobalStyles } from "~/globalStyles.style";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
