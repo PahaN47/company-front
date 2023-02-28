186d476c5f91bc9ef5a05c478c2096a18269839c
PahaN47 <pahan4781@gmail.com>
2023-03-01 01:54:26 +0300

    test commit 3

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import { AppProps } from "next/app";
-import { GlobalStyles } from "~/globalStyles.style";
+import React from 'react';
+import { AppProps } from 'next/app';
+
+import { GlobalStyles } from '~/globalStyles.style';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
-  );
+    return (
+        <>
+            <GlobalStyles />
+            <Component {...pageProps} />
+        </>
+    );
 }
