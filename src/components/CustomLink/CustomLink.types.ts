30ec9aee70c365aedbf5e1b6058c8945e6dc0384
PahaN47 <pahan4781@gmail.com>
2023-03-13 15:07:46 +0300

    add storybook

--- src/components/CustomLink/CustomLink.types.ts
+++ src/components/CustomLink/CustomLink.types.ts
@@ -1,4 +1,4 @@
-import { HTMLAttributes } from 'react';
+import { AnchorHTMLAttributes } from 'react';
 import { LinkProps } from 'next/link';
 
-export type CustomLinkProps = Omit<LinkProps, 'passHref' | 'legacyBehavior'> & HTMLAttributes<HTMLAnchorElement>;
+export type CustomLinkProps = Omit<LinkProps, 'passHref' | 'legacyBehavior'> & AnchorHTMLAttributes<HTMLAnchorElement>;
